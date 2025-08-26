114d9612c514737cd48794cef41679aae8a1bb27
Mengxi-liu <liujiyang03@gmail.com>
2025-08-26 23:29:22 +1200

    style(register): add register page and relative elements

--- frontend/src/pages/register/components/Logo.jsx
+++ frontend/src/pages/register/components/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "@/assets/img/logo.png";
+import logo from "@/assets/elements/logo.png";
 import styles from '../index.module.less';
 
 
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <img src={logo} alt="SportsBuddy Logo" className="logo-img" />
+      <img src={logo} alt="thermo Logo" className={styles.logo_img} />
     </div>
   );
 }
